08b6d69140b79fac64f32bd2f85c24f1f6980d92
Tomáš Diblík <dibla.tomas@post.cz>
2025-10-16 20:19:59 +0200

    [Technical] Migrate over to @vitejs/plugin-react-swc

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,7 +1,5 @@
 import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
-// commended out for now, because the docker build fails with @vitejs/plugin-react-swc
-// import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
